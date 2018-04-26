42377a53cc3b64da9e06c8ec3e088676265753ad
Zsolt Ero <zsolt.ero@gmail.com>
2018-04-26 02:07:58 +0200

    portrait updated

--- compare-comp.sh
+++ compare-comp.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+rm -f screens/compare*
+
 cp screens/hyperknot-landscape.cfg screens/compare.cfg
 gsed -i "s|org.xcontest.XCTrack.widget.w.WCompMap|org.xcontest.XCTrack.widget.w.WXCAssistant|g" screens/compare.cfg
 
 ./process.py screens/compare.cfg compare-nocomp -lan -nocomp
 ./process.py screens/compare.cfg compare-onlycomp -lan -onlycomp
+
+rm screens/compare.cfg
 
 gsed -i 's|"UUID".*||g' screens/compare-*.cfg
 gsed -i 's|"X1".*||g' screens/compare-*.cfg
